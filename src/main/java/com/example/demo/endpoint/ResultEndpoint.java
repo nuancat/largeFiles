ac06e5d9623c8371e63533227e793fae7e3ae61d
Shamil Bikchentaev <wirelines@yandex.ru>
2019-01-09 22:59:35 +0300

    some unimportant additionals

---
+++ src/main/java/com/example/demo/endpoint/ResultEndpoint.java
@@ -0,0 +1,7 @@
+package com.example.demo.endpoint;
+
+/**
+ * Created by Ilyas on 09.01.2019.
+ */
+public class ResultEndpoint {
+}
---
+++ src/main/java/com/example/demo/entity/Code.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity;
+
+/**
+ * Created by Ilyas on 09.01.2019.
+ */
+public enum Code {
+}
